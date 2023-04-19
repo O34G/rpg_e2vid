75604c2faa5dd45e9a2ea26c148e2967dffe19c6
O34G <82615622+O34G@users.noreply.github.com>
2023-04-19 14:47:48 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 # models: E2VID_lightweight.pth.tar, firenet_1000.pth.tar
 MODEL="pretrained/firenet_1000.pth.tar"
 INPUT_DATA="./data/dynamic_6dof.zip"
+
+FRAMERATE = 10000
+# window duration (expressed in ms)
+WINDOW_DURATION = (1 / $FRAMERATE) * 1000
+
 OUT_FOLDER="./output/"
 DATASET="dynamic_6dof"
 
@@ -11,6 +16,6 @@ python run_reconstruction.py \
   --display \
   --show_events \
   --fixed_duration \
-  --window_duration 0.1 \
+  --window_duration $WINDOW_DURATION \
   --output_folder $OUT_FOLDER \
   --dataset_name $DATASET
