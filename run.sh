e323e73468424ff1e8434d6b48497327e6a33472
O34G <82615622+O34G@users.noreply.github.com>
2023-04-19 10:59:57 +0200

    Add run.sh
    Shell script for run_reconstruction.py

---
+++ run.sh
@@ -0,0 +1,16 @@
+# models: E2VID_lightweight.pth.tar, firenet_1000.pth.tar
+MODEL="pretrained/firenet_1000.pth.tar"
+INPUT_DATA="./data/dynamic_6dof.zip"
+OUT_FOLDER="./output/"
+DATASET="dynamic_6dof"
+
+python run_reconstruction.py \
+  -c $MODEL \
+  -i $INPUT_DATA \
+  --auto_hdr \
+  --display \
+  --show_events \
+  --fixed_duration \
+  --window_duration 0.1 \
+  --output_folder $OUT_FOLDER \
+  --dataset_name $DATASET
